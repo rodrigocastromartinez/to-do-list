9d61662e498013d93a73ccc076cfaebdd4e73f69
rodrigocastromartinez <rodrigocastro.mdp@gmail.com>
2023-06-12 14:34:45 +0200

    update editPost app logic to xhr

--- staff/rodrigo-castro/fullstack/app/src/logic/editPost.js
+++ staff/rodrigo-castro/fullstack/app/src/logic/editPost.js
@@ -1,42 +1,40 @@
-// import { validateId, validateText, validateUrl } from "./helpers/validators"
-import { findPostById, findUserById, savePost } from "../data"
 import { validators } from 'com'
 
 const { validateId, validateText, validateUrl } = validators
 
 export default function editPost(userId, postId, image, text, callback) {
-    // const post = retrievePost(userId, postId)
-
     validateId(userId, 'user id')
     validateId(postId, 'post id')
     validateUrl(image, 'image url')
     validateText(text)
 
-    findUserById(userId, user => {
-        if (!user) {
-            callback(new Error(`user with id ${userId} not found`))
+    const xhr = new XMLHttpRequest
+
+    xhr.onload = () => {
+        const { status } = xhr
+
+        if (status !== 200) {
+            const { response: json } = xhr
+            const { error } = JSON.parse(json)
+
+            callback(new Error(error))
 
             return
         }
 
-        findPostById(postId, post => {
-            if (!post) {
-                callback(new Error(`post with id ${postId} not found`))
+        callback(null)
+    }
 
-                return
-            }
+    xhr.onerror = () => {
+        callback(new Error('connection error'))
+    }
 
-            if (userId !== post.author) {
-                callback(new Error(`Post doesn't belong to this user`))
+    xhr.open('PATCH', `${import.meta.env.VITE_API_URL}/posts/${userId}/${postId}`)
 
-                return
-            }
+    xhr.setRequestHeader('Content-Type', 'application/json')
 
-            post.image = image
-            post.date = new Date
-            post.text = text
+    const post = { image, text }
+    const json = JSON.stringify(post)
 
-            savePost(post, () => callback(null))
-        })
-    })
+    xhr.send(json)
 }
