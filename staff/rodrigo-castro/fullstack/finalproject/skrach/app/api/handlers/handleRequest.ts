86308ce1c316b110dced90111f4d35d6253473cf
rodrigocastromartinez <rodrigocastro.mdp@gmail.com>
2023-07-27 21:16:39 +0200

    fixed handler request error

---
+++ staff/rodrigo-castro/fullstack/finalproject/skrach/app/api/handlers/handleRequest.ts
@@ -0,0 +1,27 @@
+import dbConnect from '../../data/dbConnect'
+import { headers } from 'next/headers'
+import { NextResponse, NextRequest } from 'next/server'
+
+
+const handleRequest = async (callback: () => Promise<NextResponse<string>>) => {
+        try {
+            await dbConnect()
+            
+            const headersList = headers()
+        
+            const contentType = headersList.get('Content-Type')
+        
+            if (contentType !== 'application/json') {
+                NextResponse.json({error: 'no application/json header found'}, {status: 400})
+        
+                return
+            }
+            
+            return await callback()
+        } catch(error: any){
+            return NextResponse.json({error: error.message}, {status: 500})
+        }
+
+}
+
+export default handleConnect
--- staff/rodrigo-castro/fullstack/finalproject/skrach/app/api/users/auth/route.ts
+++ staff/rodrigo-castro/fullstack/finalproject/skrach/app/api/users/auth/route.ts
@@ -4,6 +4,7 @@ import { authenticateUser } from '../../../logic'
 import { headers } from 'next/headers'
 import jwt from 'jsonwebtoken'
 import dbConnect from '../../../data/dbConnect'
+import handleConnect from '../../handlers/handleRequest'
 
 interface RequestBody {
     name: string,
@@ -12,20 +13,12 @@ interface RequestBody {
 }
 
 export async function POST(req: NextRequest) {
-    try {
-        await dbConnect()
-
-        const headersList = headers()
-
-        const contentType = headersList.get('Content-Type')
-
-        if (contentType !== 'application/json') {
-            NextResponse.json({error: 'no application/json header found'}, {status: 400})
-
-            return
-        }
-
+    console.count()
+    return handleConnect( async () => {
+        console.count()
         const body = await req.text()
+
+        console.count()
         
         const { email, password }: RequestBody = JSON.parse(body)
 
@@ -36,9 +29,37 @@ export async function POST(req: NextRequest) {
         const { JWT_SECRET, JWT_EXPIRATION } = process.env
 
         const token = jwt.sign(payload, JWT_SECRET!, { expiresIn: JWT_EXPIRATION })
-
+        
         return NextResponse.json(token)
-    } catch(error: any){
-        return NextResponse.json({error: error.message}, {status: 500})
-    }
+    })
+    
+    // try {
+    //     await dbConnect()
+
+    //     const headersList = headers()
+
+    //     const contentType = headersList.get('Content-Type')
+
+    //     if (contentType !== 'application/json') {
+    //         NextResponse.json({error: 'no application/json header found'}, {status: 400})
+
+    //         return
+    //     }
+
+    //     const body = await req.text()
+        
+    //     const { email, password }: RequestBody = JSON.parse(body)
+
+    //     const userId = await authenticateUser(email, password)
+
+    //     const payload = { sub: userId }
+
+    //     const { JWT_SECRET, JWT_EXPIRATION } = process.env
+
+    //     const token = jwt.sign(payload, JWT_SECRET!, { expiresIn: JWT_EXPIRATION })
+
+    //     return NextResponse.json(token)
+    // } catch(error: any){
+    //     return NextResponse.json({error: error.message}, {status: 500})
+    // }
 }
