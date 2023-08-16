f1c66c740b91ce7a30a6fdfefb185cfe5ae94e93
rodrigocastromartinez <rodrigocastro.mdp@gmail.com>
2023-08-16 13:58:45 +0200

    created save url logic for api and test; update index

--- staff/rodrigo-castro/fullstack/finalproject/skrach/app/logic/index.ts
+++ staff/rodrigo-castro/fullstack/finalproject/skrach/app/logic/index.ts
@@ -3,11 +3,21 @@ import authenticateUser from './authenticateUser'
 import retrieveUser from './retrieveUser'
 import updateUserAvatar from './updateUserAvatar'
 import createProject from './createProject'
+import createTrack from './createTrack'
+import retrieveProject from './retrieveProject'
+import retrieveUserProjects from './retrieveUserProjects'
+import saveAudio from './saveAudio'
+import saveUrl from './saveUrl'
 
 export {
     registerUser,
     authenticateUser,
     retrieveUser,
     updateUserAvatar,
-    createProject
+    createProject,
+    createTrack,
+    retrieveProject,
+    retrieveUserProjects,
+    saveAudio,
+    saveUrl
 }
