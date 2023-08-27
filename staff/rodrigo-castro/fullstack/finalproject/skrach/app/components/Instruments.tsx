18e93e89a93a8a9619b6c0c513d1e8ad3b7e5f52
rodrigocastromartinez <rodrigocastro.mdp@gmail.com>
2023-08-27 18:14:21 +0200

    added close modal to instrument modal

--- staff/rodrigo-castro/fullstack/finalproject/skrach/app/components/Instruments.tsx
+++ staff/rodrigo-castro/fullstack/finalproject/skrach/app/components/Instruments.tsx
@@ -1,6 +1,7 @@
 import { TrackModel } from "../data/interfaces"
 import { Dispatch, SetStateAction } from "react"
 import { updateInstrument } from "../logic/client"
+import Button from './Button'
 
 interface InstrumentsProps {
     trackData: TrackModel
@@ -16,10 +17,12 @@ export default function Instruments({ trackData, projectId, setModal }: Instrume
         setModal(undefined)
     }
 
+    const handleCloseModal = () => setModal(undefined)
+
     return <>
     <div className="fixed top-0 left-0 h-screen w-screen bg-[var(--black-transparent)] flex flex-col justify-center items-center z-30 ">
         <div className="flex justify-center items-center h-fit w-fit relative top-0 left-0 z-50">
-            <div className="flex justify-center items-center w-80 h-fit p-4 rounded-2xl bg-[var(--grey-700)]">
+            <div className="flex flex-col gap-2 justify-center items-center w-80 h-fit p-4 rounded-2xl bg-[var(--grey-700)]">
                 <ul className="flex flex-row gap-6">
                     <li onClick={() => onInstrumentSelected('note')} ><img src="/note.svg" className={`h-8 w-8 ${trackData.instrument && trackData.instrument === 'note' ? 'filter-orange' : ''}`}/></li>
                     <li onClick={() => onInstrumentSelected('mic')}><img src="/mic.svg" className={`h-8 w-8 ${trackData.instrument && trackData.instrument === 'mic' ? 'filter-orange' : ''}`}/></li>
@@ -27,6 +30,7 @@ export default function Instruments({ trackData, projectId, setModal }: Instrume
                     <li onClick={() => onInstrumentSelected('piano')}><img src="/piano.svg" className={`h-8 w-8 ${trackData.instrument && trackData.instrument === 'piano' ? 'filter-orange' : ''}`}/></li>
                     <li onClick={() => onInstrumentSelected('drums')}><img src="/drums.svg" className={`h-8 w-8 ${trackData.instrument && trackData.instrument === 'drums' ? 'filter-orange' : ''}`}/></li>
                 </ul>
+                <Button size='wide' type='no-fill' text='Back' onClick={handleCloseModal}></Button>
             </div>
         </div>
     </div>
